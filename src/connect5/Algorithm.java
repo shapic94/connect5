02e16fdf60a91b12d79cf51bab055c0bbd895630
Nebojsa Sapic <nebojsa.sapic@Nebojsas-MacBook-Pro-2.local>
2017-08-08 20:00:41 +0200

    Uredjenje1

--- src/connect5/Algorithm.java
+++ src/connect5/Algorithm.java
@@ -7,4 +7,25 @@ public class Algorithm {
 
     private int col;
     private int row;
+
+    public Algorithm(int col, int row) {
+        this.col = col;
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
 }
