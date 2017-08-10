abd36f9348ef7fc28d5fb3062eabcdd8653573f2
astojmenovic <aleksandar.stojmenovic9@gmail.com>
2017-08-10 14:09:24 +0200

    Nova izmena

--- src/main/Main.java
+++ src/main/Main.java
@@ -15,6 +15,7 @@ public class Main {
 
     public static void main(String [] args) {
         System.out.println("Welcome to connect5 games");
+        System.out.println("test1");
 
         // Rows
         int row = 10;
